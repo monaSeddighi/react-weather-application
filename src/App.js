1416098cd6d13c8586e12ebdf6698d1a1f79631c
Mona <113529781+monaSeddighi@users.noreply.github.com>
2023-01-11 16:46:12 +0330

    adding a heading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <h2>Weather App</h2>
         <footer>
           The page was coded by <a href="https://github.com/monaSeddighi">Mona Seddighi</a> and it is an <a
           href="https://github.com/monaSeddighi/my-homework-project" target="_blank" rel="noopener noreferrer">
