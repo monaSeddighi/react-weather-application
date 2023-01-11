72a9d1426f00842a002bc8afa4cf5e06a52e65c9
Mona <113529781+monaSeddighi@users.noreply.github.com>
2023-01-11 16:42:36 +0330

    deleting unnecessary files, adding an anchor to the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <footer>
+          The page was coded by <a href="https://github.com/monaSeddighi">Mona Seddighi</a> and it is an <a
+          href="https://github.com/monaSeddighi/my-homework-project" target="_blank" rel="noopener noreferrer">
+          open-source on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
