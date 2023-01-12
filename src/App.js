38c228f614aab2152a5756501198ee1b477f1232
Mona <113529781+monaSeddighi@users.noreply.github.com>
2023-01-12 11:17:21 +0330

    adding a form to the html

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
             <div className="container">
-      <h2>Weather App</h2>
+      
       <Weather/>
         <footer>
           The page was coded by <a href="https://github.com/monaSeddighi">Mona Seddighi</a> and it is an <a
--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,13 @@ import "./Weather.css"
 export default function Weather(){
     return(
         <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9"><input type="search" placeholder="Enter a city" className="form-control"/></div>
+                
+                 <div className="col-3"><input type="submit" value="Search" className="btn btn-primary"/></div>
+                </div>
+            </form>
             <h1>New York</h1>
             <ul>
                 <li>Thursday 02:00</li>
