3291a54e5cf1fb5514af90564694d724a41c5c98
Mona <113529781+monaSeddighi@users.noreply.github.com>
2023-01-12 15:58:45 +0330

    organizing the unit, number and image

--- src/Weather.js
+++ src/Weather.js
@@ -16,11 +16,14 @@ export default function Weather(){
                 <li>Cloudy</li>
             </ul>
             <div className="row">
-                <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="weather forecast" />
-                    <span className="temperature">6</span> 
-                    <span className="unit">°C</span>
-                    </div>
+ <div className="col-6">
+<div className="d-flex">
+ <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="weather forecast" />
+       <span className="temperature">6</span> 
+     <span className="unit">°C</span>
+  </div>
+  </div>
+
                     <div className="col-6"> 
                     <ul>
                         <li> Precipitation: 2%  </li>
@@ -28,9 +31,8 @@ export default function Weather(){
                     <li> Wind: 14 km/h  </li>
                     </ul>
                     </div>
-
             </div>
-
         </div>
+        
     )
 }
