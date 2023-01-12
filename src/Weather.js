feec17509727dad6968f6ad3c3292f7d875d75c8
Mona <113529781+monaSeddighi@users.noreply.github.com>
2023-01-12 14:24:56 +0330

    styling the Weather component
    styling ul, li, h1, footer both in the Weather.css and in the App.css

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,7 @@ export default function Weather(){
                 <div className="row">
                     <div className="col-9"><input type="search" placeholder="Enter a city" className="form-control"/></div>
                 
-                 <div className="col-3"><input type="submit" value="Search" className="btn btn-primary"/></div>
+                 <div className="col-3"><input type="submit" value="Search" className="btn btn-primary w-100"/></div>
                 </div>
             </form>
             <h1>New York</h1>
