f96ebbfca3a5c3a1f6f3b9ab76a518e29d197d3c
Mona <113529781+monaSeddighi@users.noreply.github.com>
2023-01-27 13:55:46 +0100

    giving an space to the anchor

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <Weather defaultCity="New York"/>
         <footer className="footer-content">
           The project was coded by {" "}
-          <a href="https://github.com/monaSeddighi/my-homework-project">Mona Seddighi</a> and is <a href="https://github.com/monaSeddighi" target="_blank" rel="noopener noreferrer">{" "}open-sourced on GitHub {" "} </a> and is hosted on {" "}<a href="https://app.netlify.com/sites/silver-cannoli-04e796/overview">Netlify.</a>
+          <a href="https://github.com/monaSeddighi/my-homework-project">Mona Seddighi</a> and is <a href="https://github.com/monaSeddighi" target="_blank" rel="noopener noreferrer">{" "}open-sourced on GitHub</a> {" "}and is hosted on {" "}<a href="https://app.netlify.com/sites/silver-cannoli-04e796/overview">Netlify.</a>
     </footer>
       </div>
     </div>
