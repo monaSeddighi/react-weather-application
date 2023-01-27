8808db73c778e4a345cc3a9354e14abdf5c6b9d3
Mona <113529781+monaSeddighi@users.noreply.github.com>
2023-01-27 18:38:52 +0100

    building weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,21 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon"
 export default function WeatherForecast(){
-    return"this is the forecast"
+    return (
+        <div className="WeatherForecast">
+    <div className="row">
+        <div className="col">
+            <div className="WeatherForecast-day">Thu</div>
+       <WeatherIcon code="01d"/>
+       <div className="WeatherForecast-temperatures">
+<span className="WeatherForecast-temperature-max">19</span>
+<span className="WeatherForecast-temperature-min">10</span>
+       </div>
+        </div>
+    </div>
+
+
+
+        </div>
+    )
 }
